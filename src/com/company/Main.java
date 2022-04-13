b93fc68c9169aa10eaa21cb690fa082e82748621
bienpacman <biendz2000@gmail.com>
2022-04-13 14:31:04 +0700

    thuc hanh1

---
+++ src/com/company/Main.java
@@ -0,0 +1,52 @@
+package com.company;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter source file : ");
+        String sourcePath = scanner.nextLine();
+        System.out.println("Enter destination file : ");
+        String destPath = scanner.nextLine();
+
+        File sourceFile = new File(sourcePath);
+        File destFile = new File(destPath);
+        try {
+            copyFileUsingJava7Files(sourceFile,destFile);
+            System.out.println("Copy completed");
+        } catch (IOException ioe) {
+            System.out.println("Can't copy that file");
+            System.out.println(ioe.getMessage());
+        }
+    }
+
+
+    private static void copyFileUsingJava7Files(File source, File dest) throws IOException {
+        Files.copy(source.toPath(),dest.toPath(), StandardCopyOption.REPLACE_EXISTING);
+         }
+
+    private static void copyfileUsingStream(File source, File dest) throws IOException {
+        InputStream is = null;
+        OutputStream os = null;
+        try {
+            is = new FileInputStream(source);
+            os = new FileOutputStream(dest);
+
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = is.read(buffer)) > 0){
+                os.write (buffer,0,length);
+            }
+        }finally {
+            is.close();
+            os.close();
+        }
+    }
+
+
+}
